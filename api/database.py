9571bc30a640b5a41808005d95547b952c7b6d9b
Hiwot Alemayehu <151120526+life412@users.noreply.github.com>
2026-02-04 23:29:27 +0300

    Set up database connection using SQLAlchemy

--- api/database.py
+++ api/database.py
@@ -1,1 +1,22 @@
+# api/database.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_NAME = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
